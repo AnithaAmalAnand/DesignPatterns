43289ee14d91ec18e2feaf9967473c7cc8e3fb0c
Anitha Deenadayalan <adeenadayalan@acadian-asset.com>
2020-04-09 14:05:08 +0800

    Added tests

--- Patterns/Singleton.cs
+++ Patterns/Singleton.cs
@@ -1,17 +1,20 @@
 using System;
-public class SingletonService {
-    private static readonly Lazy<SingletonService> _singletonObj =   new Lazy<SingletonService>(() => new SingletonService());
-    private string uid;
-    private SingletonService() {
-        //Make constructor private to prevent instantiation and class derivation
-        uid = Guid.NewGuid().ToString();
-    }
 
-    public static SingletonService GetInstance() {
-        return _singletonObj.Value;
-    }
+namespace DesignPatterns {
+    public class SingletonService {
+        private static readonly Lazy<SingletonService> _singletonObj =   new Lazy<SingletonService>(() => new SingletonService());
+        private string uid;
+        private SingletonService() {
+            //Make constructor private to prevent instantiation and class derivation
+            uid = Guid.NewGuid().ToString();
+        }
 
-    public void PrintPattern() {
-        Console.WriteLine("I am Singleton with uid:" + uid);
+        public static SingletonService GetInstance() {
+            return _singletonObj.Value;
+        }
+
+        public void PrintPattern() {
+            Console.WriteLine("I am Singleton with uid:" + uid);
+        }
     }
 }
---
+++ Tests/SingletonShould.cs
@@ -0,0 +1,21 @@
+using Xunit;
+
+namespace DesignPatterns.Tests {
+    public class SingletonShould {
+        [Fact]
+        public void ReturnSameInstance()
+        {
+        //Given
+
+        var sObj1 = SingletonService.GetInstance();
+        var sObj2 = SingletonService.GetInstance();
+        
+        //When
+        
+        //Then
+        Assert.Same(sObj1, sObj2);
+        
+        }
+
+    }
+}
